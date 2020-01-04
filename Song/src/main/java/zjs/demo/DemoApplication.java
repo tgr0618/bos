04ad3c51259d5a446536d24153bcc2ffeac7b406
tgr0618 <56573636+tgr0618@users.noreply.github.com>
2020-01-04 14:43:07 +0800

    asd
    asd

---
+++ Song/src/main/java/zjs/demo/DemoApplication.java
@@ -0,0 +1,19 @@
+package zjs.demo;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EnableTransactionManagement
+@ComponentScan("zjs.controller")//配置扫描的控制层
+@MapperScan("zjs.mapper")//配置扫描的dao层
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
